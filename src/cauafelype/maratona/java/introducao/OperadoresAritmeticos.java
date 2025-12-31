1ca9695560d1f1417fc224a0a7bbf6828dcafffc
Cauã Felype <programandocomexcelencia@gmail.com>
2025-12-31 21:59:27 +0000

    introducao - operadores aritmeticos

---
+++ src/cauafelype/maratona/java/introducao/OperadoresAritmeticos.java
@@ -0,0 +1,20 @@
+package cauafelype.maratona.java.introducao;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+
+        // Operadores básicos aritméticos + - / *
+
+        float num1 = 1;
+        float num2 = 2;
+        float num3 = 3;
+        float num4 = 4;
+        float soma  = num3 + num4;
+        System.out.println("num1 = "+num1+", num2 = "+num2);
+        System.out.println("A soma dos numeros eh: "+ (num1+num2));
+        System.out.println("A subtracao dos numeros eh: "+ (num2-num1));
+        System.out.println("A multiplicacao dos numeros eh: "+ (num1*num2));
+        System.out.println("A divisao dos numeros eh: "+ (num1/num2));
+        System.out.println("A soma do num 3 e num 4 eh: "+ soma);
+    }
+}
