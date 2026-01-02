a813e23619a12be1d412a8a768983f08622336ce
Cauã Felype <programandocomexcelencia@gmail.com>
2026-01-02 14:40:07 -0300

    introducao - operadores de atribuicao

--- src/cauafelype/maratona/java/introducao/OperadoresLogicosAndOR.java
+++ src/cauafelype/maratona/java/introducao/OperadoresLogicosAndOR.java
@@ -9,8 +9,14 @@ public class OperadoresLogicosAndOR {
         float salario = 3500F;
         boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario > 4612;
         boolean isDentroDaLeiMenorQueTrinta = idade2 < 30 && salario >= 3381;
+        boolean isDentroDaLei = idade < 30 || salario > 3499;
         System.out.println("O trabalhador esta conforme a lei? "+ isDentroDaLeiMaiorQueTrinta);
         System.out.println("O trabalhador2 esta conforme a lei? "+ isDentroDaLeiMenorQueTrinta);
+        System.out.println("O trabalhador3 esta conforme a lei? "+ isDentroDaLei);
 
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        boolean isPs5Compravel = valorTotalContaCorrente >= 3000 || valorTotalContaPoupanca >= 3000;
+        System.out.println("Eu consigo comprar meu ps5? "+ isPs5Compravel);
     }
 }
