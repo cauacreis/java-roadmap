01fa81c413fce4d3a1314a42c5ba2bc5c60ec01f
Cauã Felype <programandocomexcelencia@gmail.com>
2026-01-04 15:54:12 -0300

    introducao - estruturas condicionais if else

--- src/cauafelype/maratona/java/introducao/EstruturasCondicionaisElseIF.java
+++ src/cauafelype/maratona/java/introducao/EstruturasCondicionaisElseIF.java
@@ -2,6 +2,21 @@ package cauafelype.maratona.java.introducao;
 
 public class EstruturasCondicionaisElseIF {
     public static void main(String[] args) {
+        
+        int idade = 10;
 
+        if (idade >= 18 && idade <= 30) {
+            System.out.println("VOCÊ É ADULTO");
+
+        } else  if (idade >= 31 && idade <= 50) {
+            System.out.println("VOCÊ É UM CÔROA");
+
+        } else  if (idade < 18) {
+            System.out.println("VOCÊ NÃO É ADULTO AINDA");
+
+        } else {
+            System.out.println("TA VELHO HEIN");
+
+        }
     }
 }
