b3edb49a6c559809f48517970b152354ae9406c2
Cauã Felype <programandocomexcelencia@gmail.com>
2026-01-06 15:50:14 -0300

    introducao - estruturas condicionais ternario

--- src/cauafelype/maratona/java/introducao/EstruturasCondicionaisElseIF.java
+++ src/cauafelype/maratona/java/introducao/EstruturasCondicionaisElseIF.java
@@ -5,22 +5,26 @@ import java.util.Scanner;
 public class EstruturasCondicionaisElseIF {
     public static void main(String[] args) {
 
+        // Boa prática: Inicializar a variável para evitar erros se o IF falhar
+        String categoria = "";
         Scanner idadeVelho = new Scanner(System.in);
         System.out.println("Digite o idade: ");
         int idade = idadeVelho.nextInt();
 
         if (idade >= 18 && idade <= 30) {
             System.out.println("VOCÊ É ADULTO");
-
+            categoria = "Adulto";
         } else  if (idade >= 31 && idade <= 50) {
             System.out.println("VOCÊ É UM CÔROA");
-
+            categoria = "Côroa";
         } else  if (idade < 18) {
             System.out.println("VOCÊ NÃO É ADULTO AINDA");
-
+            categoria = "Criança";
         } else {
             System.out.println("TA VELHO HEIN");
-
+            categoria = "Velho";
         }
+        System.out.println("Com " + idade + " anos, sua categoria é: " + categoria);
+        idadeVelho.close();
     }
 }
---
+++ src/cauafelype/maratona/java/introducao/EstruturasCondicionaisTernario.java
@@ -0,0 +1,30 @@
+package cauafelype.maratona.java.introducao;
+
+import java.util.Scanner;
+
+public class EstruturasCondicionaisTernario {
+    public static void main(String[] args) {
+        // História: Quero doar 500 reais para um instituição de caridade, mas só posso doar se meu salário por de 5000
+        // Vou primeiro mostrar como resolver da forma antiga
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Qual é seu salário: ");
+        double salario = leitor.nextDouble();
+
+        if (salario >= 5000) {
+            System.out.println("Você pode doar para a instituição de caridade");
+        } else {
+            System.out.println("Você não pode doar para a instituição de caridade :/");
+        }
+
+        // Agora da nova maneira, operador ternário
+        // (condicao) ? verdadeiro : falso
+        // Se eu receber um salário de 4500, vou investir 500 na bolsa de valores
+
+        String mensagemInvestir = "Eu vou investir 500 na bolsa de valores";
+        String mensagemNaoInvestir = "Eu não vou investir 500 na bolsa de valores";
+
+        String resultado = salario >= 5000 ? mensagemInvestir : mensagemNaoInvestir;
+        System.out.println(resultado);
+        leitor.close();
+    }
+}
