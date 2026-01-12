0382869637516c636c77711e99f877b03b5a52c7
Cauã Felype <programandocomexcelencia@gmail.com>
2026-01-12 19:27:59 -0300

    introducao - loops aninhados ex2

--- src/cauafelype/maratona/java/introducao/LoopsAninhadosEx2.java
+++ src/cauafelype/maratona/java/introducao/LoopsAninhadosEx2.java
@@ -1,7 +1,21 @@
 package cauafelype.maratona.java.introducao;
 
+import java.util.Scanner;
+
 public class LoopsAninhadosEx2 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Qual o tamanho da escada do mário? ");
+        int tamanho = input.nextInt();
+
+        for(int i = 0; i < tamanho; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
