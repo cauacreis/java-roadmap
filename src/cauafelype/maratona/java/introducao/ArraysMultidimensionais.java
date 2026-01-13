9e382c5930e46b41799072fb4b241a08a2b60a62
Cauã Felype <programandocomexcelencia@gmail.com>
2026-01-13 19:36:35 -0300

    introducao - arrays multidimensionais

--- src/cauafelype/maratona/java/introducao/ArraysMultidimensionais.java
+++ src/cauafelype/maratona/java/introducao/ArraysMultidimensionais.java
@@ -12,11 +12,12 @@ public class ArraysMultidimensionais {
         System.out.println("Me diga, qual é a coluna que você quer acessar? Limitado até o número 11: ");
         int numeroColuna = input.nextInt();
 
-        int mesesDias [][] = {
+        int[][] mesesDias = {
                 {1, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, // janeiro // índice 0
                 {1, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, // fevereiro // índice 1
                 {1, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12} // março, todos os 3 meses até o dia 12 // índice 2
         };
         System.out.println("Estamos acessando a linha: "+numeroLinha+" || Agora o dado guardado na posição :"+numeroColuna+" || O valor guardado é: "+mesesDias[numeroLinha][numeroColuna]);
+        input.close();
     }
 }
