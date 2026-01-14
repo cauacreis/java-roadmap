c1cce2b93dfbd5a69483f46babc43afd5f2f76ea
Cauã Felype <programandocomexcelencia@gmail.com>
2026-01-13 21:08:12 -0300

    introducao - arrays multidimensionais

--- src/cauafelype/maratona/java/introducao/ArraysMultidimensionais.java
+++ src/cauafelype/maratona/java/introducao/ArraysMultidimensionais.java
@@ -11,13 +11,23 @@ public class ArraysMultidimensionais {
         int numeroLinha = input.nextInt();
         System.out.println("Me diga, qual é a coluna que você quer acessar? Limitado até o número 11: ");
         int numeroColuna = input.nextInt();
-
-        int[][] mesesDias = {
-                {1, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, // janeiro // índice 0
-                {1, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, // fevereiro // índice 1
-                {1, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12} // março, todos os 3 meses até o dia 12 // índice 2
+        //o primeiro [] representa as linhas e o segundo representa as colunas
+        int[][] linhasColunas = {
+                {1, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, //  // índice 0
+                {1, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, //  // índice 1
+                {1, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}  //  // índice 2
         };
-        System.out.println("Estamos acessando a linha: "+numeroLinha+" || Agora o dado guardado na posição :"+numeroColuna+" || O valor guardado é: "+mesesDias[numeroLinha][numeroColuna]);
+        System.out.println("Estamos acessando a linha: "+numeroLinha+" || Agora o dado guardado na posição :"+numeroColuna+" || O valor guardado é: "+linhasColunas[numeroLinha][numeroColuna]);
+        for (int[] linhasColuna : linhasColunas) {
+            for (int i : linhasColuna) {
+                System.out.println("Esses são os dados dentro dos arrays: " + i);
+            }
+        }
+        for (int i = 0; i < linhasColunas.length; i++) {
+            for (int j = 0; j < linhasColunas[i].length; j++) {
+                System.out.println("Esses são os dados dentro dos arrays, segunda versão: "+linhasColunas[i][j]);
+            }
+        }
         input.close();
     }
 }
