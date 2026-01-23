1443c3f816169918514ef37875e8f04c088cfef7
Cauã Felype <programandocomexcelencia@gmail.com>
2026-01-22 21:48:34 -0300

    introducao - Palindromo

---
+++ src/cauafelype/maratona/java/introducao/Palindromo.java
@@ -0,0 +1,33 @@
+package cauafelype.maratona.java.introducao;
+
+import java.util.Scanner;
+
+//Palindromo do jeito menos eficiente   
+
+public class Palindromo {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o palíndromo: ");
+        String palindromo = input.nextLine();
+        char[] palindromoLetras = palindromo.toCharArray();
+        int ponteiroInicial = 0;
+        int ponteiroFinal = palindromo.length() - 1;
+
+        for (int i = 0; i < palindromoLetras.length; i++) {
+            System.out.println("Verificando palíndromo, letra 1: || "+palindromoLetras[ponteiroInicial]+ " || letra 2: "+palindromoLetras[ponteiroFinal]);
+            if (palindromoLetras[ponteiroInicial] == palindromoLetras[ponteiroFinal]) {
+                ponteiroInicial++;
+                ponteiroFinal--;
+
+            }  else {
+                System.out.println("Isto não é um palíndromo! ");
+                return;
+            }
+        }
+
+        System.out.println("Parabéns você digitou um palíndromo");
+
+    }
+}
