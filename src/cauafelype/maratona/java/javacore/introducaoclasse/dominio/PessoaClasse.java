67eeb6797af5012cc455a46442418092a6870bca
Cauã Felype <programandocomexcelencia@gmail.com>
2026-01-24 19:15:11 -0300

    POO - Introducao Classes

---
+++ src/cauafelype/maratona/java/javacore/introducaoclasse/dominio/PessoaClasse.java
@@ -0,0 +1,7 @@
+package cauafelype.maratona.java.javacore.introducaoclasse.dominio;
+
+public class PessoaClasse {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/cauafelype/maratona/java/javacore/introducaoclasse/testes/PessoaTeste.java
@@ -0,0 +1,15 @@
+package cauafelype.maratona.java.javacore.introducaoclasse.testes;
+
+import cauafelype.maratona.java.javacore.introducaoclasse.dominio.PessoaClasse;
+
+public class PessoaTeste {
+    public static void main(String[] args) {
+        PessoaClasse pessoa = new PessoaClasse();
+        pessoa.idade = 20;
+        pessoa.sexo = 'F';
+        pessoa.nome = "Maria";
+        System.out.println(pessoa.nome);
+        System.out.println(pessoa.sexo);
+        System.out.println(pessoa.idade);
+    }
+}
