36687ed7661d34bbb72d1accc354e527f132759b
Cauã Felype <programandocomexcelencia@gmail.com>
2026-01-30 17:22:44 -0300

    POO - Introducao Metodos Parametros

--- src/cauafelype/maratona/java/javacore/introducaometodos/dominio/calculadora.java
+++ src/cauafelype/maratona/java/javacore/introducaometodos/dominio/calculadora.java
@@ -7,4 +7,8 @@ public class calculadora {
     public void subtraiDoisNumeros(){
         System.out.println(15-10);
     }
+
+    public void multiplicaDoisNumeros(int num1, int num2){
+        System.out.println(num1*num2);
+    }
 }
--- src/cauafelype/maratona/java/javacore/introducaometodos/teste/CalculadoraTeste.java
+++ src/cauafelype/maratona/java/javacore/introducaometodos/teste/CalculadoraTeste.java
@@ -1,13 +1,24 @@
 package cauafelype.maratona.java.javacore.introducaometodos.teste;
 
 import cauafelype.maratona.java.javacore.introducaometodos.dominio.calculadora;
+import java.util.Scanner;
 
 public class CalculadoraTeste {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         calculadora somaDosNumeros = new calculadora();
         somaDosNumeros.somaDoisNumeros();
         System.out.println("Finalizando CalculadoraTeste");
         somaDosNumeros.subtraiDoisNumeros();
         System.out.println("Finalizando Subtracao");
+        System.out.println("Me diga o primeiro número para multiplicar: ");
+        int num1 = input.nextInt();
+        System.out.println("Me diga o primeiro número para multiplicar: ");
+        int num2 = input.nextInt();
+
+        somaDosNumeros.multiplicaDoisNumeros(num1, num2);
+        System.out.println("Finalizando Multiplicacao");
     }
 }
