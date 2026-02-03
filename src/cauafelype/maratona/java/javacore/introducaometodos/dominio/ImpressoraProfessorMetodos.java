05298ec3b3c6bdc986f84469048b7136886e4c46
Cauã Felype <programandocomexcelencia@gmail.com>
2026-02-03 14:19:30 -0300

    POO - Introducao Metodos Parametros Tipo referencia 2

--- src/cauafelype/maratona/java/javacore/introducaometodos/dominio/ImpressoraProfessorMetodos.java
+++ src/cauafelype/maratona/java/javacore/introducaometodos/dominio/ImpressoraProfessorMetodos.java
@@ -2,7 +2,19 @@ package cauafelype.maratona.java.javacore.introducaometodos.dominio;
 
 public class ImpressoraProfessorMetodos {
     public void imprime(ProfessorMetodo professorMetodo) {
-        System.out.println(professorMetodo.nomeProfessor+" "+professorMetodo.salaProfessor+" "+professorMetodo.materiaProfessor+" "+professorMetodo.sexoProfessor+" "+professorMetodo.idadeProfessor);
+        System.out.println(professorMetodo.nomeProfessor + " " + professorMetodo.salaProfessor + " " + professorMetodo.materiaProfessor + " " + professorMetodo.sexoProfessor + " " + professorMetodo.idadeProfessor);
+
+    }
+
+    public void imprime2(ProfessorMetodo professorMetodo) {
+        professorMetodo.nomeProfessor = "Maria";
+        System.out.println(professorMetodo.nomeProfessor + " " + professorMetodo.salaProfessor + " " + professorMetodo.materiaProfessor + " " + professorMetodo.sexoProfessor + " " + professorMetodo.idadeProfessor);
+
+    }
+
+    public void imprime3(ProfessorMetodo professorMetodo) {
+        System.out.println(professorMetodo.nomeProfessor + " " + professorMetodo.salaProfessor + " " + professorMetodo.materiaProfessor + " " + professorMetodo.sexoProfessor + " " + professorMetodo.idadeProfessor);
+        professorMetodo.nomeProfessor = "Maria";
 
     }
 }
--- src/cauafelype/maratona/java/javacore/introducaometodos/teste/ProfessorTeste.java
+++ src/cauafelype/maratona/java/javacore/introducaometodos/teste/ProfessorTeste.java
@@ -7,6 +7,7 @@ public class ProfessorTeste {
     public static void main(String[] args) {
         ProfessorMetodo professorMetodo01 = new ProfessorMetodo();
         ProfessorMetodo professorMetodo02 = new ProfessorMetodo();
+        ProfessorMetodo professorMetodo03 = new ProfessorMetodo();
         ImpressoraProfessorMetodos impressora = new ImpressoraProfessorMetodos();
         // Duas variaveis professorMetodo01 e 02, que estao fazendo referência ao objeto ProfessorMetodo
 
@@ -22,10 +23,22 @@ public class ProfessorTeste {
         professorMetodo02.idadeProfessor = 22;
         professorMetodo02.materiaProfessor = "Artes";
 
+        professorMetodo03.nomeProfessor = "Ua";
+        professorMetodo03.salaProfessor = "H 203";
+        professorMetodo03.sexoProfessor = 'M';
+        professorMetodo03.idadeProfessor = 22;
+        professorMetodo03.materiaProfessor = "Artes";
+
         System.out.println(professorMetodo01.nomeProfessor+" "+professorMetodo01.salaProfessor+" "+professorMetodo01.materiaProfessor+" "+professorMetodo01.sexoProfessor+" "+professorMetodo01.idadeProfessor);
         System.out.println("--------------");
         impressora.imprime(professorMetodo02);
-
-
+        System.out.println("---------------");
+        System.out.println("Esse metodo obrigatoriamente coloca o nome como maria, para sempre");
+        impressora.imprime2(professorMetodo02);
+        System.out.println("---------------");
+        System.out.println("Agora vamos ver quando ele altera o nome depois de executar: ");
+        impressora.imprime3(professorMetodo03);
+        System.out.println("----------------");
+        impressora.imprime3(professorMetodo03);
     }
 }
