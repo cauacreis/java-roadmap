a0e6af66b2ce6836f4d237fb6183022866d75382
Cauã Felype <programandocomexcelencia@gmail.com>
2026-02-07 21:01:24 -0300

    POO - Introducao Exercicio Salarios

--- src/cauafelype/maratona/java/javacore/introducaoclasse/testes/FuncionarioTeste.java
+++ src/cauafelype/maratona/java/javacore/introducaoclasse/testes/FuncionarioTeste.java
@@ -1,4 +1,13 @@
 package cauafelype.maratona.java.javacore.introducaoclasse.testes;
 
+import cauafelype.maratona.java.javacore.introducaoclasse.dominio.FuncionarioClasse;
+
 public class FuncionarioTeste {
+    public static void main(String[] args) {
+        FuncionarioClasse f = new FuncionarioClasse();
+        f.nome = "Maria";
+        f.idade = 20;
+        System.out.println("Nome do funcionario: " + f.nome);
+        System.out.println("Idade do funcionario: " + f.idade);
+    }
 }
