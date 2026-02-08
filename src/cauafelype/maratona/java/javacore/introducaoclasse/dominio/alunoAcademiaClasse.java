0c3067916e0e547a797927a863b366777304f5fd
Cauã Felype <programandocomexcelencia@gmail.com>
2026-02-08 18:27:34 -0300

    Desafio app de academia

---
+++ src/cauafelype/maratona/java/javacore/introducaoclasse/dominio/alunoAcademiaClasse.java
@@ -0,0 +1,10 @@
+package cauafelype.maratona.java.javacore.introducaoclasse.dominio;
+
+public class alunoAcademiaClasse {
+    public String nome;
+    public int idade;
+    public double peso;
+    public int altura;
+    public int frequenciaAtividade;
+    public char sexo;
+}
