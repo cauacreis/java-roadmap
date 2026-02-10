65559400fc1195a09c8b4618a6c1ff156111c53a
Cauã Felype <programandocomexcelencia@gmail.com>
2026-02-10 13:59:07 -0300

    Desafio app de academia embelezamento de código

--- src/cauafelype/maratona/java/javacore/introducaometodos/dominio/AcademiaHealthMonitor.java
+++ src/cauafelype/maratona/java/javacore/introducaometodos/dominio/AcademiaHealthMonitor.java
@@ -4,9 +4,10 @@ import cauafelype.maratona.java.javacore.introducaoclasse.dominio.AlunoAcademiaC
 
 public class AcademiaHealthMonitor {
     public static void calculadoraIMC(AlunoAcademiaClasse aluno) {
-         aluno.imc = aluno.peso / (aluno.alturaEmMetros * aluno.alturaEmMetros);
+        aluno.imc = aluno.peso / (aluno.alturaEmMetros * aluno.alturaEmMetros);
         System.out.printf("O IMC do aluno %s é: %.2f%n", aluno.nome, aluno.imc);
     }
+
     public static void classificacaoDeIMC(AlunoAcademiaClasse aluno) {
         if (aluno.imc < 18.5) {
             System.out.printf("A classificação do aluno %s é: Magreza%n", aluno.nome);
@@ -24,18 +25,20 @@ public class AcademiaHealthMonitor {
             System.out.printf("A classificação do aluno %s é: Obesidade Grave%n", aluno.nome);
         }
     }
-    public static void taxaMetabolicaBasal (AlunoAcademiaClasse aluno) {
+
+    public static void taxaMetabolicaBasal(AlunoAcademiaClasse aluno) {
         int alturaEmCm;
         alturaEmCm = (int) (aluno.alturaEmMetros * 100);
         if (aluno.sexo.equalsIgnoreCase("M")) {
             aluno.taxaMetabolicaTMB = 66 + (13.7 * aluno.peso) + (5 * alturaEmCm) - (6.8 * aluno.idade);
-            System.out.println("Essa é sua taxa metabólica basal: "+aluno.taxaMetabolicaTMB);
+            System.out.println("Essa é sua taxa metabólica basal: " + aluno.taxaMetabolicaTMB);
         } else {
             aluno.taxaMetabolicaTMB = 655 + (9.6 * aluno.peso) + (1.8 * alturaEmCm) - (4.7 * aluno.idade);
-            System.out.println("Essa é sua taxa metabólica basal: "+aluno.taxaMetabolicaTMB+"KCAL");
+            System.out.println("Essa é sua taxa metabólica basal: " + aluno.taxaMetabolicaTMB + "KCAL");
         }
     }
-    public static void fatorDeAtividade (AlunoAcademiaClasse aluno) {
+
+    public static void fatorDeAtividade(AlunoAcademiaClasse aluno) {
         double fatorAtividade = switch (aluno.frequenciaAtividade) {
             case 2 -> 1.375;
             case 3 -> 1.55;
@@ -45,11 +48,13 @@ public class AcademiaHealthMonitor {
         aluno.gastoTotal = aluno.taxaMetabolicaTMB * fatorAtividade;
         System.out.printf("Você gasta em média essa quantidade de calorias por dia, baseado no seu nível de atividade: %.2f KCAL%n", aluno.gastoTotal);
     }
-    public static void consumirAgua (AlunoAcademiaClasse aluno) {
+
+    public static void consumirAgua(AlunoAcademiaClasse aluno) {
         double agua = aluno.peso * 50;
-        System.out.println("Você deve consumir essa quantidade de água por dia: "+agua+"ML");
+        System.out.println("Você deve consumir essa quantidade de água por dia: " + agua + "ML");
     }
-    public static void objetivo (AlunoAcademiaClasse aluno) {
+
+    public static void objetivo(AlunoAcademiaClasse aluno) {
         double caloriasObjetivo;
         switch (aluno.objetivo) {
             case 1 -> {
@@ -63,10 +68,11 @@ public class AcademiaHealthMonitor {
                 System.out.printf("O máximo de bulking a ser alcançado é: %.2f KCAL%n", caloriasObjetivo);
             }
             case 3 ->
-                System.out.printf("Apenas consuma a quantidade necessária de calorias que você necessita: %.2f KCAL%n ", aluno.gastoTotal);
+                    System.out.printf("Apenas consuma a quantidade necessária de calorias que você necessita: %.2f KCAL%n ", aluno.gastoTotal);
         }
     }
-    public static void treinoAluno (AlunoAcademiaClasse aluno) {
+
+    public static void treinoAluno(AlunoAcademiaClasse aluno) {
         switch (aluno.objetivo) {
             case 1 -> {
                 System.out.println("OBJETIVO: EMAGRECIMENTO (PERDA DE GORDURA)");
@@ -117,7 +123,8 @@ public class AcademiaHealthMonitor {
 
         }
     }
-    public static void divisaoDeTexto () {
+
+    public static void divisaoDeTexto() {
         System.out.println("--------------------------");
     }
 }
