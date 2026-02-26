a558db15fc8759b394cc609f6986772a58a2c0cb
Cauã Felype <programandocomexcelencia@gmail.com>
2026-02-26 15:40:13 -0300

    introducao poo - modificador static

---
+++ src/cauafelype/maratona/java/javacore/modificadorestatico/dominio/Carro.java
@@ -0,0 +1,9 @@
+package cauafelype.maratona.java.javacore.modificadorestatico.dominio;
+
+public class Carro {
+    private String marca;
+    private String modelo;
+    private double velocidadeMax;
+    final double velocidadeLimite = 250;
+    // final significa que é imutável
+}
