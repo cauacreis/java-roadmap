592c2027773dac095ddf22c099f673c300d6a037
Cauã Felype <programandocomexcelencia@gmail.com>
2026-02-27 15:41:14 -0300

    introducao poo - modificador static - constante

--- src/cauafelype/maratona/java/javacore/modificadorestatico/dominio/Carro.java
+++ src/cauafelype/maratona/java/javacore/modificadorestatico/dominio/Carro.java
@@ -4,6 +4,45 @@ public class Carro {
     private String marca;
     private String modelo;
     private double velocidadeMax;
-    final double velocidadeLimite = 250;
-    // final significa que é imutável
+    public static final double VELOCIDADE_LIMITE = 250;
+    // final significa que é imutável e não pertence ao objetio e sim a classe, ela é uma constante
+    // por isso ela é escrita em letra maíuscula e com underline
+    // mas você pode utilizar ele sem o final, para caso você deseja alterar o valor dele, fora do código domínio
+
+    public Carro(String marca, String modelo, double velocidadeMax) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMax = velocidadeMax;
+    }
+
+    public void imprime() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Velocidade max: " + velocidadeMax+ " Km/h");
+        System.out.println("Velocidade limite do acordo alemão entre cavalheiros: " + VELOCIDADE_LIMITE + " Km/h");
+        System.out.println("--------------------------------------------------------------------");
+    }
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getVelocidadeMax() {
+        return velocidadeMax;
+    }
+
+    public void setVelocidadeMax(double velocidadeMax) {
+        this.velocidadeMax = velocidadeMax;
+    }
 }
---
+++ src/cauafelype/maratona/java/javacore/modificadorestatico/teste/CarroTeste01.java
@@ -0,0 +1,13 @@
+package cauafelype.maratona.java.javacore.modificadorestatico.teste;
+
+import cauafelype.maratona.java.javacore.modificadorestatico.dominio.Carro;
+
+public class CarroTeste01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro("Volkswagen", "T-Cross", 280);
+        Carro carro02 = new Carro("Volkswagen", "Fusca", 110);
+        carro.imprime();
+
+        carro02.imprime();
+    }
+}
