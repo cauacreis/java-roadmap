b6eb2bac0db1a344f29eee1623abd14835981e0a
Cauã Felype <programandocomexcelencia@gmail.com>
2026-03-06 15:35:38 -0300

    poo - bloco de inicializacao estatico

---
+++ src/cauafelype/maratona/java/javacore/modificadorestatico/dominio/anime.java
@@ -0,0 +1,39 @@
+package cauafelype.maratona.java.javacore.modificadorestatico.dominio;
+
+public class anime {
+    String nome;
+    private static int[] eps;
+
+     // 0 - bloco de inicializacao é executado quando a JVM carregar a classe
+     // 1 - alocado espaço em memória pro objeto
+     // 2 - cada atributo da classe é criado e inicializado com valores default ou o que for definido
+     // 3- bloco de inicializacao é executado
+     // 4- construtor é executado
+    static {
+        System.out.println("Dentro do bloco de inicialização");
+        eps = new int[100];
+        for (int i = 0; i < eps.length; i++) {
+            eps[i] = i+1;
+        }
+    }
+
+    public anime(String nome) {
+        this.nome = nome;
+    }
+
+    public anime() {
+        System.out.print("Episódios: ");
+        for (int i = 0; i < eps.length; i++) {
+            System.out.print(" "+eps[i]);
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEps() {
+        return eps;
+    }
+}
---
+++ src/cauafelype/maratona/java/javacore/modificadorestatico/teste/AnimeTeste01.java
@@ -0,0 +1,10 @@
+package cauafelype.maratona.java.javacore.modificadorestatico.teste;
+
+import cauafelype.maratona.java.javacore.modificadorestatico.dominio.anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        anime anime1 = new anime();
+        anime anime2 = new anime();
+    }
+}
