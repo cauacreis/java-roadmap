a7cdfd884e2b0a436939c4e99ab66e49b212f0c8
Cauã Felype <programandocomexcelencia@gmail.com>
2026-04-16 19:43:26 -0300

    poo - introducao heranca exercicio robocode robos criados

--- src/cauafelype/maratona/java/javacore/heranca/dominio/RoboDefesa.java
+++ src/cauafelype/maratona/java/javacore/heranca/dominio/RoboDefesa.java
@@ -1,14 +1,14 @@
 package cauafelype.maratona.java.javacore.heranca.dominio;
 
 public final class RoboDefesa extends Robo{
+    private int nivelEscudo;
 
-
-    public RoboDefesa(String nome,) {
+    public RoboDefesa(String nome,  int nivelEscudo) {
         super(nome);
     }
 
     @Override
     public void atacar() {
-
+        System.out.println(""+this.nome+" ativou o escudo nível: "+nivelEscudo+"DEF"+" e contra-atacou!");
     }
 }
