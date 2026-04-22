5b2726956b6b58665cff25d5a332071fbaa0b19c
Cauã Felype <programandocomexcelencia@gmail.com>
2026-04-21 23:09:51 -0300

    poo introducao heranca sobescrita toString

--- src/cauafelype/maratona/java/javacore/sobescrita/teste/AnimeTeste01.java
+++ src/cauafelype/maratona/java/javacore/sobescrita/teste/AnimeTeste01.java
@@ -5,6 +5,6 @@ import cauafelype.maratona.java.javacore.sobescrita.dominio.Anime;
 public class AnimeTeste01 {
     public static void main(String[] args) {
         Anime bokuNoHero = new Anime("Boku No Hero");
-        System.out.println(bokuNoHero);
+        System.out.println(bokuNoHero); // pode ser bokuNoHero ou bokuNoHero.toString() que vai funcionar!
     }
 }
