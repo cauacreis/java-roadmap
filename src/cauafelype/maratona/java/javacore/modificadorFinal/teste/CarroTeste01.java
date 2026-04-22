7a2befac59537445e080bfe85aa9864d5559bec9
Cauã Felype <programandocomexcelencia@gmail.com>
2026-04-22 15:16:26 -0300

    poo introducao heranca referencia

---
+++ src/cauafelype/maratona/java/javacore/modificadorFinal/teste/CarroTeste01.java
@@ -0,0 +1,12 @@
+package cauafelype.maratona.java.javacore.modificadorFinal.teste;
+
+import cauafelype.maratona.java.javacore.modificadorFinal.dominio.Carro;
+
+public class CarroTeste01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.setNome("Hillux");
+        System.out.println("Nome: " + carro.getNome());
+        System.out.println("Velocidade limite: "+Carro.VELOCIDADE_LIMITE);
+    }
+}
