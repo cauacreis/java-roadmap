7c7d0417ec873ba280e0aabb69fc151bc9437b27
Cauã Felype <programandocomexcelencia@gmail.com>
2026-04-23 15:33:25 -0300

    poo introducao heranca enum

--- src/cauafelype/maratona/java/javacore/enumeracao/teste/ClienteTeste01.java
+++ src/cauafelype/maratona/java/javacore/enumeracao/teste/ClienteTeste01.java
@@ -4,7 +4,7 @@ import cauafelype.maratona.java.javacore.enumeracao.dominio.Cliente;
 
 public class ClienteTeste01 {
     public static void main(String[] args) {
-        Cliente pedroCliente = new Cliente("Pedro", "Pessoa_fisica");
+        Cliente pedroCliente = new Cliente("Pedro", "Pessoa_fisicaa");
         System.out.println(pedroCliente);
     }
 }
