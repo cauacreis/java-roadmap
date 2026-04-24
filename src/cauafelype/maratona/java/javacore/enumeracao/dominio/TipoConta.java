1e8561820bee9bad7a94b2e58682080dae35789e
Cauã Felype <programandocomexcelencia@gmail.com>
2026-04-24 14:53:50 -0300

    poo introducao heranca enum

--- src/cauafelype/maratona/java/javacore/enumeracao/dominio/TipoConta.java
+++ src/cauafelype/maratona/java/javacore/enumeracao/dominio/TipoConta.java
@@ -11,3 +11,10 @@ public enum TipoConta {
 // porque se o banco inventar a "Conta Salário", você não precisa abrir a classe Conta original
 // e botar mais um else if. Você só cria um filho novo! Mas para sistemas menores,
 // a Opção A (com Enum) é super rápida e resolve o problema muito bem.
+
+// o enum pode estar dentro de uma classe normal tbm, por exemplo:
+// public class Cliente {
+// public enum TipoConta {
+// POUPANCA, CORRENTE, SALARIO
+// }
+// só que é um jeito mais feito e mais desorganizado não tão comum usado para utilizar enum
