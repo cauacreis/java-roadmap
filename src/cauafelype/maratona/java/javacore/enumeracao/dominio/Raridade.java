37cfacbb33ef9e48fb48b44b0f34f72742dc4bbb
Cauã Felype <programandocomexcelencia@gmail.com>
2026-04-25 21:49:25 -0300

    poo introducao heranca enum

---
+++ src/cauafelype/maratona/java/javacore/enumeracao/dominio/Raridade.java
@@ -0,0 +1,18 @@
+package cauafelype.maratona.java.javacore.enumeracao.dominio;
+
+public enum Raridade {
+    MILITAR(1.5),
+    RESTRITO(3.0),
+    SECRETO(5.5),
+    OCULTO(10.0),
+
+    private double multiplicadorDePreco;
+
+    Raridade(double multiplicadorDePreco) {
+        this.multiplicadorDePreco = multiplicadorDePreco;
+    }
+
+    public double getMultiplicadorDePreco() {
+        return multiplicadorDePreco;
+    }
+}
