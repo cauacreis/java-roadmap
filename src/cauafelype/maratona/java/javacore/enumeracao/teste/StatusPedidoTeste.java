b01b1992c9c057d66cd2a8c87bd2a2ef667443a2
Cauã Felype <programandocomexcelencia@gmail.com>
2026-04-30 15:38:06 -0300

    poo introducao heranca enum

---
+++ src/cauafelype/maratona/java/javacore/enumeracao/teste/StatusPedidoTeste.java
@@ -0,0 +1,10 @@
+package cauafelype.maratona.java.javacore.enumeracao.teste;
+import cauafelype.maratona.java.javacore.enumeracao.dominio.StatusPedido;
+
+public class StatusPedidoTeste {
+    public static void main(String[] args) {
+        int numeroDoBanco = 3;
+        StatusPedido statusPedido = StatusPedido.buscaPorCodigo(numeroDoBanco);
+        System.out.println("O status do seu pedido eh: "+ statusPedido);
+    }
+}
