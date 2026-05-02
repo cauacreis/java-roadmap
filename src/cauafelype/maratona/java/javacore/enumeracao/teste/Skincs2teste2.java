27a234876384461d453314c6e178b49780bd5ee3
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-01 23:29:33 -0300

    lista

--- src/cauafelype/maratona/java/javacore/enumeracao/teste/Skincs2teste2.java
+++ src/cauafelype/maratona/java/javacore/enumeracao/teste/Skincs2teste2.java
@@ -1,4 +1,5 @@
 package cauafelype.maratona.java.javacore.enumeracao.teste;
+
 import java.util.ArrayList;
 import java.util.List;
 import cauafelype.maratona.java.javacore.enumeracao.dominio.Raridade;
@@ -7,69 +8,88 @@ import cauafelype.maratona.java.javacore.enumeracao.dominio.SkinCS2;
 public class Skincs2teste2 {
     public static void main(String[] args) {
         List<SkinCS2> carrinhoDeCompras = new ArrayList<>();
-        SkinCS2 skin1 = new SkinCS2(120.0, "AK-47 Redline", Raridade.RESTRITO);
-        SkinCS2 skin2 = new SkinCS2(850.0, "AWP Dragon Lore", Raridade.OCULTO);
-        SkinCS2 skin3 = new SkinCS2(300.0, "M4A1-S Hyper Beast", Raridade.SECRETO);
-        SkinCS2 skin4 = new SkinCS2(90.0, "USP-S Cortex", Raridade.RESTRITO);
-        SkinCS2 skin5 = new SkinCS2(45.0, "Glock-18 Water Elemental", Raridade.MILITAR);
-        SkinCS2 skin6 = new SkinCS2(60.0, "Desert Eagle Conspiracy", Raridade.RESTRITO);
-        SkinCS2 skin7 = new SkinCS2(150.0, "AK-47 Frontside Misty", Raridade.SECRETO);
-        SkinCS2 skin8 = new SkinCS2(700.0, "Karambit Fade", Raridade.OCULTO);
-        SkinCS2 skin9 = new SkinCS2(200.0, "M4A4 Neo-Noir", Raridade.SECRETO);
-        SkinCS2 skin10 = new SkinCS2(80.0, "Famas Roll Cage", Raridade.MILITAR);
 
-        SkinCS2 skin11 = new SkinCS2(110.0, "Galil AR Chatterbox", Raridade.RESTRITO);
-        SkinCS2 skin12 = new SkinCS2(500.0, "Butterfly Knife Slaughter", Raridade.OCULTO);
-        SkinCS2 skin13 = new SkinCS2(130.0, "AK-47 Phantom Disruptor", Raridade.SECRETO);
-        SkinCS2 skin14 = new SkinCS2(70.0, "P250 See Ya Later", Raridade.RESTRITO);
-        SkinCS2 skin15 = new SkinCS2(40.0, "MP9 Hydra", Raridade.MILITAR);
-        SkinCS2 skin16 = new SkinCS2(210.0, "AWP Neo-Noir", Raridade.SECRETO);
-        SkinCS2 skin17 = new SkinCS2(600.0, "M9 Bayonet Doppler", Raridade.OCULTO);
-        SkinCS2 skin18 = new SkinCS2(95.0, "SG 553 Pulse", Raridade.RESTRITO);
-        SkinCS2 skin19 = new SkinCS2(55.0, "UMP-45 Primal Saber", Raridade.MILITAR);
-        SkinCS2 skin20 = new SkinCS2(175.0, "AK-47 Neon Rider", Raridade.SECRETO);
+        List<SkinCS2> pacoteDeArmas = List.of(
+                new SkinCS2(120.0, "AK-47 Redline", Raridade.RESTRITO),
+                new SkinCS2(850.0, "AWP Dragon Lore", Raridade.OCULTO),
+                new SkinCS2(300.0, "M4A1-S Hyper Beast", Raridade.SECRETO),
+                new SkinCS2(90.0, "USP-S Cortex", Raridade.RESTRITO),
+                new SkinCS2(45.0, "Glock-18 Water Elemental", Raridade.MILITAR),
+                new SkinCS2(60.0, "Desert Eagle Conspiracy", Raridade.RESTRITO),
+                new SkinCS2(150.0, "AK-47 Frontside Misty", Raridade.SECRETO),
+                new SkinCS2(700.0, "Karambit Fade", Raridade.OCULTO),
+                new SkinCS2(200.0, "M4A4 Neo-Noir", Raridade.SECRETO),
+                new SkinCS2(80.0, "Famas Roll Cage", Raridade.MILITAR),
+                new SkinCS2(110.0, "Galil AR Chatterbox", Raridade.RESTRITO),
+                new SkinCS2(500.0, "Butterfly Knife Slaughter", Raridade.OCULTO),
+                new SkinCS2(130.0, "AK-47 Phantom Disruptor", Raridade.SECRETO),
+                new SkinCS2(70.0, "P250 See Ya Later", Raridade.RESTRITO),
+                new SkinCS2(40.0, "MP9 Hydra", Raridade.MILITAR),
+                new SkinCS2(210.0, "AWP Neo-Noir", Raridade.SECRETO),
+                new SkinCS2(600.0, "M9 Bayonet Doppler", Raridade.OCULTO),
+                new SkinCS2(95.0, "SG 553 Pulse", Raridade.RESTRITO),
+                new SkinCS2(55.0, "UMP-45 Primal Saber", Raridade.MILITAR),
+                new SkinCS2(175.0, "AK-47 Neon Rider", Raridade.SECRETO),
+                new SkinCS2(65.0, "Five-SeveN Angry Mob", Raridade.RESTRITO),
+                new SkinCS2(720.0, "Talon Knife Marble Fade", Raridade.OCULTO),
+                new SkinCS2(140.0, "M4A1-S Decimator", Raridade.SECRETO),
+                new SkinCS2(50.0, "MAC-10 Disco Tech", Raridade.MILITAR),
+                new SkinCS2(85.0, "CZ75-Auto Xiangliu", Raridade.RESTRITO),
+                new SkinCS2(190.0, "AWP Fever Dream", Raridade.SECRETO),
+                new SkinCS2(800.0, "Karambit Tiger Tooth", Raridade.OCULTO),
+                new SkinCS2(100.0, "Famas Mecha Industries", Raridade.RESTRITO),
+                new SkinCS2(35.0, "PP-Bizon High Roller", Raridade.MILITAR),
+                new SkinCS2(160.0, "AK-47 Legion of Anubis", Raridade.SECRETO),
+                new SkinCS2(75.0, "Tec-9 Fuel Injector", Raridade.RESTRITO),
+                new SkinCS2(900.0, "Butterfly Knife Crimson Web", Raridade.OCULTO),
+                new SkinCS2(220.0, "M4A4 The Emperor", Raridade.SECRETO),
+                new SkinCS2(48.0, "MP7 Bloodsport", Raridade.MILITAR),
+                new SkinCS2(115.0, "Desert Eagle Mecha Industries", Raridade.RESTRITO),
+                new SkinCS2(250.0, "AWP Containment Breach", Raridade.SECRETO),
+                new SkinCS2(650.0, "Bayonet Gamma Doppler", Raridade.OCULTO),
+                new SkinCS2(88.0, "Nova Hyper Beast", Raridade.RESTRITO),
+                new SkinCS2(42.0, "Sawed-Off Apocalypto", Raridade.MILITAR),
+                new SkinCS2(180.0, "AK-47 Bloodsport", Raridade.SECRETO),
+                new SkinCS2(95.0, "Glock-18 Vogue", Raridade.RESTRITO),
+                new SkinCS2(780.0, "Skeleton Knife Fade", Raridade.OCULTO),
+                new SkinCS2(205.0, "M4A1-S Player Two", Raridade.SECRETO),
+                new SkinCS2(38.0, "P90 Shapewood", Raridade.MILITAR),
+                new SkinCS2(125.0, "USP-S Kill Confirmed", Raridade.RESTRITO),
+                new SkinCS2(260.0, "AWP Wildfire", Raridade.SECRETO),
+                new SkinCS2(820.0, "Karambit Lore", Raridade.OCULTO),
+                new SkinCS2(92.0, "Dual Berettas Cobra Strike", Raridade.RESTRITO),
+                new SkinCS2(47.0, "XM1014 Incinegator", Raridade.MILITAR),
+                new SkinCS2(170.0, "AK-47 Vulcan", Raridade.SECRETO) // O último não tem vírgula!
+        );
 
-        SkinCS2 skin21 = new SkinCS2(65.0, "Five-SeveN Angry Mob", Raridade.RESTRITO);
-        SkinCS2 skin22 = new SkinCS2(720.0, "Talon Knife Marble Fade", Raridade.OCULTO);
-        SkinCS2 skin23 = new SkinCS2(140.0, "M4A1-S Decimator", Raridade.SECRETO);
-        SkinCS2 skin24 = new SkinCS2(50.0, "MAC-10 Disco Tech", Raridade.MILITAR);
-        SkinCS2 skin25 = new SkinCS2(85.0, "CZ75-Auto Xiangliu", Raridade.RESTRITO);
-        SkinCS2 skin26 = new SkinCS2(190.0, "AWP Fever Dream", Raridade.SECRETO);
-        SkinCS2 skin27 = new SkinCS2(800.0, "Karambit Tiger Tooth", Raridade.OCULTO);
-        SkinCS2 skin28 = new SkinCS2(100.0, "Famas Mecha Industries", Raridade.RESTRITO);
-        SkinCS2 skin29 = new SkinCS2(35.0, "PP-Bizon High Roller", Raridade.MILITAR);
-        SkinCS2 skin30 = new SkinCS2(160.0, "AK-47 Legion of Anubis", Raridade.SECRETO);
+        carrinhoDeCompras.addAll(pacoteDeArmas);
 
-        SkinCS2 skin31 = new SkinCS2(75.0, "Tec-9 Fuel Injector", Raridade.RESTRITO);
-        SkinCS2 skin32 = new SkinCS2(900.0, "Butterfly Knife Crimson Web", Raridade.OCULTO);
-        SkinCS2 skin33 = new SkinCS2(220.0, "M4A4 The Emperor", Raridade.SECRETO);
-        SkinCS2 skin34 = new SkinCS2(48.0, "MP7 Bloodsport", Raridade.MILITAR);
-        SkinCS2 skin35 = new SkinCS2(115.0, "Desert Eagle Mecha Industries", Raridade.RESTRITO);
-        SkinCS2 skin36 = new SkinCS2(250.0, "AWP Containment Breach", Raridade.SECRETO);
-        SkinCS2 skin37 = new SkinCS2(650.0, "Bayonet Gamma Doppler", Raridade.OCULTO);
-        SkinCS2 skin38 = new SkinCS2(88.0, "Nova Hyper Beast", Raridade.RESTRITO);
-        SkinCS2 skin39 = new SkinCS2(42.0, "Sawed-Off Apocalypto", Raridade.MILITAR);
-        SkinCS2 skin40 = new SkinCS2(180.0, "AK-47 Bloodsport", Raridade.SECRETO);
-
-        SkinCS2 skin41 = new SkinCS2(95.0, "Glock-18 Vogue", Raridade.RESTRITO);
-        SkinCS2 skin42 = new SkinCS2(780.0, "Skeleton Knife Fade", Raridade.OCULTO);
-        SkinCS2 skin43 = new SkinCS2(205.0, "M4A1-S Player Two", Raridade.SECRETO);
-        SkinCS2 skin44 = new SkinCS2(38.0, "P90 Shapewood", Raridade.MILITAR);
-        SkinCS2 skin45 = new SkinCS2(125.0, "USP-S Kill Confirmed", Raridade.RESTRITO);
-        SkinCS2 skin46 = new SkinCS2(260.0, "AWP Wildfire", Raridade.SECRETO);
-        SkinCS2 skin47 = new SkinCS2(820.0, "Karambit Lore", Raridade.OCULTO);
-        SkinCS2 skin48 = new SkinCS2(92.0, "Dual Berettas Cobra Strike", Raridade.RESTRITO);
-        SkinCS2 skin49 = new SkinCS2(47.0, "XM1014 Incinegator", Raridade.MILITAR);
-        SkinCS2 skin50 = new SkinCS2(170.0, "AK-47 Vulcan", Raridade.SECRETO);
-        carrinhoDeCompras.add(ak47);
-        carrinhoDeCompras.add(m4a4);
         System.out.println("\n🛒 --- SEU CARRINHO DE COMPRAS ---");
         System.out.println(" 🔹 Total de itens: " + carrinhoDeCompras.size());
+
         carrinhoDeCompras.forEach(skin ->
                 System.out.println(" 🔹 " + skin.getNome() +
                         " | Raridade: " + skin.getRaridades() +
                         " | Valor Base: R$ " + skin.getPrecoBase() +
                         " | Valor c/ Taxa: R$ " + skin.calcularPrecoSkin())
-        );        System.out.println("----------------------------------\n");
+        );
+        System.out.println("\n----------------------------------");
+        System.out.println(" 🔹 Agora as skins OCULTAS: ");
+        carrinhoDeCompras.stream()
+                        .filter(skin -> skin.getRaridades() == Raridade.OCULTO)
+                        .forEach(skin -> System.out.println(" 🔪 " + skin.getNome() + " | R$ " + skin.calcularPrecoSkin()));
+                        System.out.println("----------------------------------\n");
+        List<SkinCS2> skinsDeElite = carrinhoDeCompras.stream()
+        .filter(skin -> skin.getRaridades() == Raridade.OCULTO)
+                .toList();
+        System.out.println("\n----------------------------------");
+        System.out.println(" 🔹 Agora as skins na nova LISTA: ");
+        skinsDeElite.forEach((skin ->
+                System.out.println(" 🔹 " + skin.getNome() +
+                        " | Raridade: " + skin.getRaridades() +
+                        " | Valor Base: R$ " + skin.getPrecoBase() +
+                        " | Valor c/ Taxa: R$ " + skin.calcularPrecoSkin())
+            )
+        );
     }
 }
