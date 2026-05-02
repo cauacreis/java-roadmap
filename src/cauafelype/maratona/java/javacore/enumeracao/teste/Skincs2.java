45bf70ca0b0e4671a9128fda562a04269c3351b0
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-01 22:55:16 -0300

    lista

--- src/cauafelype/maratona/java/javacore/enumeracao/teste/Skincs2.java
+++ src/cauafelype/maratona/java/javacore/enumeracao/teste/Skincs2.java
@@ -1,11 +1,16 @@
 package cauafelype.maratona.java.javacore.enumeracao.teste;
+import java.util.ArrayList;
+import java.util.List;
 
 import cauafelype.maratona.java.javacore.enumeracao.dominio.Raridade;
 import cauafelype.maratona.java.javacore.enumeracao.dominio.SkinCS2;
 
 public class Skincs2 {
     public static void main(String[] args) {
+        List<SkinCS2> carrinhoDeCompras = new ArrayList<>();
         SkinCS2 ak47 = new SkinCS2(199.00, "AK 47 Black Laminate", Raridade.MILITAR);
-        System.out.println(ak47);
+        SkinCS2 m4a4 = new SkinCS2(399, "M4A4 Asiimov", Raridade.MILITAR);
+        carrinhoDeCompras.add(ak47);
+        carrinhoDeCompras.add(m4a4);
     }
 }
