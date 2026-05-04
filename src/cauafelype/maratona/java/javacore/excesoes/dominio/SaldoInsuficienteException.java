2b3ab1de7c590678ec9f633af5fca24e92b617b9
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-04 15:08:51 -0300

    introducao exceptions

---
+++ src/cauafelype/maratona/java/javacore/excesoes/dominio/SaldoInsuficienteException.java
@@ -0,0 +1,7 @@
+package cauafelype.maratona.java.javacore.excesoes.dominio;
+
+public class SaldoInsuficienteException extends RuntimeException {
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
+}
