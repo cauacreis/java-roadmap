552e0ca6cb77291db9d05c2ef48269f6159d0640
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-04 20:02:26 -0300

    introducao exceptions

--- src/cauafelype/maratona/java/javacore/excesoes/teste/ContaTeste02.java
+++ src/cauafelype/maratona/java/javacore/excesoes/teste/ContaTeste02.java
@@ -9,11 +9,13 @@ public class ContaTeste02 {
         Conta contaFelype = new Conta("Felype", 100, TipoConta.CORRENTE);
         try {
             contaFelype.sacar(50);
-            contaFelype.sacar(100);
-            System.out.println("Saque de 100 realizado com sucesso!");
+            contaFelype.sacar(5);
+            System.out.println("Saque de 5 realizado com sucesso!");
         } catch (SaldoInsuficienteException e) {
             System.out.println("Sistema do banco detectou um erro: ");
             System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Encerrando e salvando log do server do banco!");
         }
         System.out.println("O saldo final da sua conta é: "+contaFelype.getSaldo());
         System.out.println("Sistema sendo encerrado...");
