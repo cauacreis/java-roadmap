638195101209688c59d5e1106b6c7f05fddca83e
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-06 20:57:57 -0300

    introducao exceptions

--- src/cauafelype/maratona/java/javacore/excesoes/teste/TesteFileReader03.java
+++ src/cauafelype/maratona/java/javacore/excesoes/teste/TesteFileReader03.java
@@ -1,6 +1,5 @@
 package cauafelype.maratona.java.javacore.excesoes.teste;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLException;
