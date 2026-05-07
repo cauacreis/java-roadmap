fce466e745e133d29871dbcfaeba60f5ac6b3f43
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-07 15:08:07 -0300

    poo atividade devrentals

---
+++ src/cauafelype/maratona/java/devrentals/dominio/Equipamentos.java
@@ -0,0 +1,7 @@
+package cauafelype.maratona.java.devrentals.dominio;
+
+public abstract class Equipamentos {
+    private String nome;
+    private double valorDiaria;
+    private boolean disponivel = true;
+}
