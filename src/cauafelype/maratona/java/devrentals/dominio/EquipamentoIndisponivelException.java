2a9ffc78f923e0384d7020de6e3b0635450072aa
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-07 15:28:02 -0300

    poo atividade devrentals

---
+++ src/cauafelype/maratona/java/devrentals/dominio/EquipamentoIndisponivelException.java
@@ -0,0 +1,7 @@
+package cauafelype.maratona.java.devrentals.dominio;
+
+public class EquipamentoIndisponivelException extends RuntimeException {
+    public EquipamentoIndisponivelException(String message) {
+        super(message);
+    }
+}
