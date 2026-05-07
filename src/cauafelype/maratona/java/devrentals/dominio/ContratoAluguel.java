17064bfcd0202f4fde98dd7745e22dd0811d7755
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-07 15:33:17 -0300

    poo atividade devrentals

---
+++ src/cauafelype/maratona/java/devrentals/dominio/ContratoAluguel.java
@@ -0,0 +1,10 @@
+package cauafelype.maratona.java.devrentals.dominio;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ContratoAluguel {
+    private Equipamentos equipamentos;
+    private LocalDate dataRetirada;
+    private localDate dataDevolucao;
+}
