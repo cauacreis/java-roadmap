5249bfccbbaa078c88425d21846da00adb8272d3
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-08 19:36:28 -0300

    poo atividade devrentals

--- src/cauafelype/maratona/java/devrentals/dominio/ContratoAluguel.java
+++ src/cauafelype/maratona/java/devrentals/dominio/ContratoAluguel.java
@@ -27,6 +27,24 @@ public class ContratoAluguel {
         return data.format(formatter);
     }
 
+    public void processarDevolucao(LocalDate dataRealDevolucao) {
+        System.out.println("Esta é a data de devolução do seu equipamento: "+formatarData(dataDevolucao));
+
+        if (dataRealDevolucao.isBefore(dataDevolucao)) {
+            long diasAtraso =  ChronoUnit.DAYS.between(dataDevolucao, dataRealDevolucao);
+
+            double valorMultaPorDia = getEquipamentos().getValorDiaria() * 1.50;
+            double totalMulta = valorMultaPorDia * diasAtraso;
+
+            System.out.println("Atraso detectado, quantidade de dias atrasados: "+diasAtraso);
+            System.out.println("Este é o valor total a ser pago: "+totalMulta);
+        } else {
+            long diasAlugados = ChronoUnit.DAYS.between(dataRetirada, dataDevolucao);
+            System.out.println("Equipamento devolvido dentro da data de devolução\n preço sem multas: "+getEquipamentos().getValorDiaria() * diasAlugados);
+        }
+
+    }
+
     public Equipamentos getEquipamentos() {
         return equipamentos;
     }
