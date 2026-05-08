5a45d45211c3bda70b4b9d06e53081e6f971fbf8
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-08 15:29:51 -0300

    poo atividade devrentals

--- src/cauafelype/maratona/java/devrentals/teste/DevRentalsTeste01.java
+++ src/cauafelype/maratona/java/devrentals/teste/DevRentalsTeste01.java
@@ -37,17 +37,14 @@ public class DevRentalsTeste01 {
         System.out.println("\nDevRentals: Sistema operando normalmente.");
         Equipamentos outraCaraTentantoAlugarAMesmaPlaca = lista.get(0);
         try {
-           ContratoAluguel alugarPlacaDeVideo2 = new ContratoAluguel(outraCaraTentantoAlugarAMesmaPlaca, hoje, devolucao7Dias);
-            if (equipamentoParaAlugar.isDisponivel()) {
-                System.out.println("Equipamento: " + alugarPlacaDeVideo2.getEquipamentos().getNome());
-                System.out.println("Esta é a data de retirada: " + alugarPlacaDeVideo2.getDataRetirada());
-                System.out.println("Esta é a data para devolução: " + alugarPlacaDeVideo2.getDataDevolucao());
-                System.out.println("Este é o preço a pagar pelo aluguel de 7 dias: " + alugarPlacaDeVideo2.calcularValorTotal());
-                equipamentoParaAlugar.setDisponivel(false);
-            } else {
+            if (!equipamentoParaAlugar.isDisponivel()) {
                 throw new EquipamentoIndisponivelException("Este equipamento não está disponível! ");
+            } else {
+                ContratoAluguel alugarPlacaDeVideo2 = new ContratoAluguel(outraCaraTentantoAlugarAMesmaPlaca, hoje, devolucao7Dias);
+                equipamentoParaAlugar.setDisponivel(false);
+                System.out.println("Aluguel realizado com sucesso! Valor: R$ " + alugarPlacaDeVideo2.calcularValorTotal());
             }
-            } catch (EquipamentoIndisponivelException e) {
+        } catch (EquipamentoIndisponivelException e) {
                 System.out.println(e.getMessage());
             }
     }
