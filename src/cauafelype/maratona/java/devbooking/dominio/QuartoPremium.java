8c353d0d82d54453e0839bef0f8b6e37bf90e4e7
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-14 15:34:05 -0300

    devbooking exercicio

--- src/cauafelype/maratona/java/devbooking/dominio/QuartoPremium.java
+++ src/cauafelype/maratona/java/devbooking/dominio/QuartoPremium.java
@@ -2,4 +2,9 @@ package cauafelype.maratona.java.devbooking.dominio;
 
 public class QuartoPremium extends Quarto{
     private boolean temHidromassagem;
+
+    public QuartoPremium(int numeroQuarto, double valorDiaria, boolean temHidromassagem) {
+        super(numeroQuarto, valorDiaria);
+        this.temHidromassagem = temHidromassagem;
+    }
 }
