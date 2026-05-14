33ebfbe9d895a3e7bc68b68800e651a0f6d44ce1
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-14 19:35:20 -0300

    devbooking exercicio

--- src/cauafelype/maratona/java/devbooking/teste/QuartosTeste01.java
+++ src/cauafelype/maratona/java/devbooking/teste/QuartosTeste01.java
@@ -3,6 +3,7 @@ package cauafelype.maratona.java.devbooking.teste;
 import cauafelype.maratona.java.devbooking.dominio.Quarto;
 import cauafelype.maratona.java.devbooking.dominio.QuartoPremium;
 import cauafelype.maratona.java.devbooking.dominio.QuartoStandard;
+import cauafelype.maratona.java.devbooking.dominio.StatusQuarto;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +11,8 @@ import java.util.List;
 public class QuartosTeste01 {
     public static void main(String[] args) {
         List<Quarto> quartos = new ArrayList<>();
-        Quarto normal = new QuartoStandard(103, 299.99, false);
-        Quarto premium = new QuartoPremium(103, 299.99, true);
+        Quarto normal = new QuartoStandard(103, 299.99, StatusQuarto.LIVRE, false);
+        Quarto premium = new QuartoPremium(103, 299.99, StatusQuarto.LIVRE, true);
 
         quartos.add(normal);
         quartos.add(premium);
