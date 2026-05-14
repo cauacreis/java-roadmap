63a9d9663e5084bda27d81019a9aa0c6dfca2574
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-14 15:26:43 -0300

    devbooking exercicio

---
+++ src/cauafelype/maratona/java/devbooking/dominio/DatasInvalidasException.java
@@ -0,0 +1,7 @@
+package cauafelype.maratona.java.devbooking.dominio;
+
+public class DatasInvalidasException extends RuntimeException {
+    public DatasInvalidasException(String message) {
+        super(message);
+    }
+}
