1dd4f398a2b67a5a450c1b30d4043e486e3bfe0f
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-14 15:30:26 -0300

    devbooking exercicio

---
+++ src/cauafelype/maratona/java/devbooking/dominio/Reserva.java
@@ -0,0 +1,10 @@
+package cauafelype.maratona.java.devbooking.dominio;
+
+import java.time.LocalDate;
+
+public class Reserva {
+    private int quarto;
+    private String nomeHospede;
+    private LocalDate dataCheckIn;
+    private LocalDate dataCheckOut;
+}
