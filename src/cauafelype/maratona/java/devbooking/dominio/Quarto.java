99caf04f480765140c3fc838c7ae7c2f16faf0d9
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-14 15:33:46 -0300

    devbooking exercicio

--- src/cauafelype/maratona/java/devbooking/dominio/Quarto.java
+++ src/cauafelype/maratona/java/devbooking/dominio/Quarto.java
@@ -3,4 +3,17 @@ package cauafelype.maratona.java.devbooking.dominio;
 public abstract class Quarto {
     int numeroQuarto;
     double valorDiaria;
+
+    public Quarto(int numeroQuarto, double valorDiaria) {
+        this.numeroQuarto = numeroQuarto;
+        this.valorDiaria = valorDiaria;
+    }
+
+    public int getNumeroQuarto() {
+        return numeroQuarto;
+    }
+
+    public double getValorDiaria() {
+        return valorDiaria;
+    }
 }
--- src/cauafelype/maratona/java/devbooking/dominio/Reserva.java
+++ src/cauafelype/maratona/java/devbooking/dominio/Reserva.java
@@ -1,10 +1,17 @@
 package cauafelype.maratona.java.devbooking.dominio;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Reserva {
     private int quarto;
     private String nomeHospede;
     private LocalDate dataCheckIn;
     private LocalDate dataCheckOut;
+
+    public double calcularTotal (){
+        long diasReservas = ChronoUnit.DAYS.between(dataCheckIn, dataCheckOut);
+        double total = quarto*diasReservas;
+        return total;
+    }
 }
