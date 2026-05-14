899c758fa4e60e16f9f374df9fda7e1a7c92de64
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-14 15:34:31 -0300

    devbooking exercicio

--- src/cauafelype/maratona/java/devbooking/dominio/QuartoStandard.java
+++ src/cauafelype/maratona/java/devbooking/dominio/QuartoStandard.java
@@ -2,4 +2,9 @@ package cauafelype.maratona.java.devbooking.dominio;
 
 public class QuartoStandard extends Quarto{
     private boolean vistaParaOmar;
+
+    public QuartoStandard(int numeroQuarto, double valorDiaria,  boolean vistaParaOmar) {
+        super(numeroQuarto, valorDiaria);
+        this.vistaParaOmar = vistaParaOmar;
+    }
 }
