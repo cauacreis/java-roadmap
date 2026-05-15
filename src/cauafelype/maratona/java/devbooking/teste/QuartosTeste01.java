04e2f258b0f6305b51285585dff6ec33516b6a6d
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-15 20:15:55 -0300

    devbooking exercicio

--- src/cauafelype/maratona/java/devbooking/teste/QuartosTeste01.java
+++ src/cauafelype/maratona/java/devbooking/teste/QuartosTeste01.java
@@ -35,5 +35,34 @@ public class QuartosTeste01 {
         } catch (QuartoIndisponivelException e) {
             System.out.println(e.getMessage());
         }
+        Quarto quartosParaReservar2 = quartos.get(0);
+
+        try {
+            if(quartosParaReservar2.getStatusQuarto() != StatusQuarto.LIVRE) {
+                throw new QuartoIndisponivelException("Este quarto está ocupado ou em manutenção, espere ele estar livre!");
+            }
+            Reserva quartoParaJoao = new Reserva(quartosParaReservar2, "Joao", hoje, dataReserva);
+            System.out.println("Esta é a data do seu CheckIn:"+quartoParaJoao.getDataCheckIn());
+            System.out.println("Esta é a data do seu CheckOut:"+quartoParaJoao.getDataCheckOut());
+            quartoParaJoao.calcularTotal();
+            quartosParaReservar2.setStatusQuarto(StatusQuarto.OCUPADO);
+            System.out.println(quartosParaReservar2.getStatusQuarto());
+        } catch (QuartoIndisponivelException e) {
+            System.out.println(e.getMessage());
+        }
+        Quarto quartosParaReservar3 = quartos.get(1);
+
+        try {
+            if (quartosParaReservar3.getStatusQuarto() != StatusQuarto.LIVRE) {
+                throw new QuartoIndisponivelException("Este quarto está indisponível!");
+            }
+            LocalDate dataReservaPraTras = hoje.minusDays(7);
+            Reserva quartoParaJose = new Reserva(quartosParaReservar3, "Jose", hoje, dataReservaPraTras);
+            System.out.println("Esta é a data do seu CheckIn:"+quartoParaJose.getDataCheckIn());
+            System.out.println("Esta é a data do seu CheckOut:"+quartoParaJose.getDataCheckOut());
+            quartoParaJose.calcularTotal();
+            quartosParaReservar3.setStatusQuarto(StatusQuarto.OCUPADO);
+            System.out.println(quartosParaReservar3.getStatusQuarto());
+        }
     }
 }
