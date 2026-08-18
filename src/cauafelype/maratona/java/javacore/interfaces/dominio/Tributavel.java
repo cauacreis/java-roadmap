acb81fcf8353021bf891c97a74362c787349e42a
Cauã Felype <programandocomexcelencia@gmail.com>
2026-08-18 14:55:36 -0300

    java - interfaces 1

--- src/cauafelype/maratona/java/javacore/interfaces/dominio/Tributavel.java
+++ src/cauafelype/maratona/java/javacore/interfaces/dominio/Tributavel.java
@@ -4,4 +4,7 @@ public interface Tributavel {
     // todo mundo que der implement nisso, tem que saber calcular o imposto
     double calcularImposto();
     String getDescricaoDoItem();
+    default void emitirRecibo() {
+        System.out.println("Emitindo recibo para o governo para: "+getDescricaoDoItem());
+    }
 }
