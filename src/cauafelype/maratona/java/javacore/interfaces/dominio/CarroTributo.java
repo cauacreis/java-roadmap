6fe441df64f8df6c2884126f93a070a3e46613d1
Cauã Felype <programandocomexcelencia@gmail.com>
2026-08-18 13:46:56 -0300

    java - interfaces 1

--- src/cauafelype/maratona/java/javacore/interfaces/dominio/CarroTributo.java
+++ src/cauafelype/maratona/java/javacore/interfaces/dominio/CarroTributo.java
@@ -1,4 +1,24 @@
 package cauafelype.maratona.java.javacore.interfaces.dominio;
 
-public class CarroTributo {
+public class CarroTributo implements Tributavel {
+    private double valor;
+    private String modelo;
+
+    public CarroTributo(double valor, String modelo) {
+        this.valor = valor;
+        this.modelo = modelo;
+    }
+
+    @Override
+    public double calcularImposto() {
+        return this.valor * 0.05;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
 }
