c1e011d2715012fc7dd466d2b812fea4c6937cb2
Cauã Felype <programandocomexcelencia@gmail.com>
2026-08-19 14:34:48 -0300

    java annotations]

--- src/cauafelype/maratona/java/javacore/annotations/desafio/dominio/ValidadorDeObjetos.java
+++ src/cauafelype/maratona/java/javacore/annotations/desafio/dominio/ValidadorDeObjetos.java
@@ -1,7 +1,26 @@
 package cauafelype.maratona.java.javacore.annotations.desafio.dominio;
 
-public class ValidadorDeObjetos {
-    public static void validar(Object objeto) throws IllegalArgumentException {
+import java.lang.reflect.Field;
 
+public class ValidadorDeObjetos {
+    public static void validar(Object objeto) throws IdadeMinimaException, IllegalAccessException {
+        Class classe = objeto.getClass();
+        Field[] variaveis = classe.getDeclaredFields();
+
+        for (Field variavel : variaveis) {
+            if (variavel.isAnnotationPresent(IdadeMinima.class)) {
+                IdadeMinima regra = variavel.getAnnotation(IdadeMinima.class);
+                int idadeExigida = regra.valor();
+
+                variavel.setAccessible(true);
+
+                int idadeDoCliente = (int) variavel.get(objeto);
+
+                if  (idadeExigida > idadeDoCliente) {
+                    throw new RuntimeException("🚨 BARRADO! A variável '" + variavel.getName() +
+                            "' exige no mínimo " + idadeExigida + " anos. O cliente tem " + idadeDoCliente + ".");
+                    }
+                }
+            }
+        }
     }
-}
